adb375eb4097b26cae3fc6e51a0052da4c78b424
Pedram Najafizadeh <82384259+pedramnajafi@users.noreply.github.com>
2023-01-14 21:14:19 +0330

    Add files via upload

---
+++ Reading a text file.py
@@ -0,0 +1,11 @@
+text_file = open('inputFile.txt', 'r')
+
+
+for line in text_file:
+    line_split = line.split() # divide all the columns
+    if line_split[2] == 'P': # if the 3rd column is P, print the whole line
+        print(line)
+
+text_file.close()
+
+
