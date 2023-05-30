f93459588dd63a0dc82943bc87349733b2c9f399
Pedram Najafizadeh <82384259+pedramnajafi@users.noreply.github.com>
2023-05-30 15:09:18 +0330

    Update Reading a text file.py

--- Reading a text file.py
+++ Reading a text file.py
@@ -4,7 +4,6 @@ for line in text_file:
     line_split = line.split() # divide all the columns
     if line_split[2] == 'P': # if the 3rd column is P, print the whole line
         print(line)
-
 text_file.close()
 
 
